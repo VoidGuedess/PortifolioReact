536e216018f4b63e9a87f7093b4fb7bd610e9194
Guedes <guedznfs@gmail.com>
2023-11-23 19:58:30 -0300

    coisas que nn lembro q eu fiz, mas é isso ai

--- src/App.jsx
+++ src/App.jsx
@@ -2,54 +2,73 @@ import TituloPagina from "./componentes/TituloPagina"
 import RowFotos from "./componentes/RowFotos"
 import UltimaFoto from "./componentes/UltimaFoto"
 
+
 export default function App(){
+
+var ftDetaque = 0;
+
+  const fotoDestaque = () => {
+    ftDetaque = document.getElementById("numero_destaque").value
+    if (ftDetaque < 0){
+      document.getElementById("numero_destaque").value = 31
+    }
+    if (ftDetaque > 31){
+      document.getElementById("numero_destaque").value = 0
+    }
+    ftDetaque = document.getElementById("numero_destaque").value
+  }
+
   return(
     <div style={{backgroundColor: "black", margin: 0, padding: 15}}>
       <br/>
       <TituloPagina/>
       <br/>
       <br/>
-      <RowFotos
-        imagem1="dia1.jpg" titulo1="Dia 1 - Cristal" imagem1Link="https://i.pinimg.com/564x/65/c6/0f/65c60fe0bef1e9342ca0c9a9efefadf6.jpg"
-        imagem2="dia2.jpg" titulo2="Dia 2 - Traje/Terno" imagem2Link="https://i.pinimg.com/564x/0b/54/e3/0b54e358e3d84e7e8f2fafad6c786a8c.jpg"
-        imagem3="dia3.jpg" titulo3="Dia 3 - Navio" imagem3Link=""
-        imagem4="dia4.jpg" titulo4="Dia 4 - Nó" imagem4Link=""
-        imagem5="dia5.jpg" titulo5="Dia 5 - Corvo" imagem5Link=""
+      <div style={{backgroundColor: "#585658", padding: 15, marginLeft: 500, marginRight: 500}}>
+          <input id="numero_destaque" style={{padding: 15, marginLeft: 125, marginRight: 125}} onChange={() => fotoDestaque()} type="number"></input>
+      </div>
+      <br/>
+      <RowFotos imgDestaque={ftDetaque}
+        id1="1" imagem1="dia1.jpg" titulo1="Dia 1 - Cristal" imagem1Link="https://i.pinimg.com/564x/65/c6/0f/65c60fe0bef1e9342ca0c9a9efefadf6.jpg"
+        id2="2" imagem2="dia2.jpg" titulo2="Dia 2 - Traje/Terno" imagem2Link="https://i.pinimg.com/564x/0b/54/e3/0b54e358e3d84e7e8f2fafad6c786a8c.jpg"
+        id3="3" imagem3="dia3.jpg" titulo3="Dia 3 - Navio" imagem3Link=""
+        id4="4" imagem4="dia4.jpg" titulo4="Dia 4 - Nó" imagem4Link=""
+        id5="5" imagem5="dia5.jpg" titulo5="Dia 5 - Corvo" imagem5Link=""
       />
       <RowFotos
-        imagem1="dia6.jpg" titulo1="Dia 6 - Espírito"
-        imagem2="dia7.jpg" titulo2="Dia 7 - Ventilador"
-        imagem3="dia8.jpg" titulo3="Dia 8 - Relógio"
-        imagem4="dia9.jpg" titulo4="Dia 9 - Pressão"
-        imagem5="dia10.jpg" titulo5="Dia 10 - Picareta/Pegar"
+        id1="6" imagem1="dia6.jpg" titulo1="Dia 6 - Espírito"
+        id2="7" imagem2="dia7.jpg" titulo2="Dia 7 - Ventilador"
+        id3="8" imagem3="dia8.jpg" titulo3="Dia 8 - Relógio"
+        id4="id9" imagem4="dia9.jpg" titulo4="Dia 9 - Pressão"
+        id5="id10" imagem5="dia10.jpg" titulo5="Dia 10 - Picareta/Pegar"
       />
       <RowFotos
-        imagem1="dia11.jpg" titulo1="Dia 11 - Azedo"
-        imagem2="dia12.jpg" titulo2="Dia 12 - Emperrado/Preso"
-        imagem3="dia13.jpg" titulo3="Dia 13 - Teto/Telhado"
-        imagem4="dia14.jpg" titulo4="Dia 14 - Tick"
-        imagem5="dia15.jpg" titulo5="Dia 15 - Capacete"
+        id1="id11" imagem1="dia11.jpg" titulo1="Dia 11 - Azedo"
+        id2="id12" imagem2="dia12.jpg" titulo2="Dia 12 - Emperrado/Preso"
+        id3="id13" imagem3="dia13.jpg" titulo3="Dia 13 - Teto/Telhado"
+        id4="id14" imagem4="dia14.jpg" titulo4="Dia 14 - Tick"
+        id5="id15" imagem5="dia15.jpg" titulo5="Dia 15 - Capacete"
       />
       <RowFotos
-        imagem1="dia16.jpg" titulo1="Dia 16 - Bússula"
-        imagem2="dia17.jpg" titulo2="Dia 17 - Colidir"
-        imagem3="dia18.jpg" titulo3="Dia 18 - Lua"
-        imagem4="dia19.jpg" titulo4="Dia 19 - Loop"
-        imagem5="dia20.jpg" titulo5="Dia 20 - Broto"
+        id1="id16" imagem1="dia16.jpg" titulo1="Dia 16 - Bússula"
+        id2="id17" imagem2="dia17.jpg" titulo2="Dia 17 - Colidir"
+        id3="id18" imagem3="dia18.jpg" titulo3="Dia 18 - Lua"
+        id4="id19" imagem4="dia19.jpg" titulo4="Dia 19 - Loop"
+        id5="id20" imagem5="dia20.jpg" titulo5="Dia 20 - Broto"
       />
       <RowFotos
-        imagem1="dia21.jpg" titulo1="Dia 21 - Peludo"
-        imagem2="dia22.jpg" titulo2="Dia 22 - Abrir"
-        imagem3="dia23.jpg" titulo3="Dia 23 - Vazar"
-        imagem4="dia24.jpg" titulo4="Dia 24 - Extinto"
-        imagem5="dia25.jpg" titulo5="Dia 25 - Splat"
+        id1="id21" imagem1="dia21.jpg" titulo1="Dia 21 - Peludo"
+        id2="id22" imagem2="dia22.jpg" titulo2="Dia 22 - Abrir"
+        id3="id23" imagem3="dia23.jpg" titulo3="Dia 23 - Vazar"
+        id4="id24" magem4="dia24.jpg" titulo4="Dia 24 - Extinto"
+        id5="id25" imagem5="dia25.jpg" titulo5="Dia 25 - Splat"
       />
       <RowFotos
-        imagem1="dia26.jpg" titulo1="Dia 26 - Conectar"
-        imagem2="dia27.jpg" titulo2="Dia 27 - Fagulha/Faísca"
-        imagem3="dia28.jpg" titulo3="Dia 28 - Crocante"
-        imagem4="dia29.jpg" titulo4="Dia 29 - Tapa-Olho"
-        imagem5="dia30.jpg" titulo5="Dia 30 - Minhoca"
+        id1="id26" imagem1="dia26.jpg" titulo1="Dia 26 - Conectar"
+        id2="id27" imagem2="dia27.jpg" titulo2="Dia 27 - Fagulha/Faísca"
+        id3="id28" imagem3="dia28.jpg" titulo3="Dia 28 - Crocante"
+        id4="id29" imagem4="dia29.jpg" titulo4="Dia 29 - Tapa-Olho"
+        id5="id30" imagem5="dia30.jpg" titulo5="Dia 30 - Minhoca"
       />
       <UltimaFoto
         imagem="dia31.jpg" titulo="Dia 31 - Arriscar"
--- src/componentes/RowFotos.jsx
+++ src/componentes/RowFotos.jsx
@@ -1,35 +1,58 @@
 export default function RowFotos(props){
+
+    function tick() {
+      //VerificaDestaque();
+    }
+    
+    setInterval(tick, 1000);
+
+    const VerificaDestaque = () => {
+      window.alert()
+      if (document.getElementById("divRow" + props.imgDestaque).id){
+        window.alert(document.getElementById("divRow" + props.imgDestaque).id)
+      }
+    }
+
     const abreDesenho = (url) => {
       window.open(url, "_blank", "noreferrer")
+    }
+    
+    const AumentarBotao = (id) => {
+      document.getElementById(id).style.height = "35px"
+      document.getElementById(id).style.color = "green"
+    }
+    const DiminuirBotao = (id) => {
+      document.getElementById(id).style.height = "25px"
+      document.getElementById(id).style.color = "black"
     }
 
     return(
       <div style={{backgroundColor: "black", placeContent:"center", padding: 15}}>
         <div style={{display: 'flex', flexDirection: 'row', alignItems: "center", placeContent:"center"}}>
-          <div style={{backgroundColor: "#585658", padding: 15, margin: 5}}>
+          <div id={"divRow" + props.id1} style={{backgroundColor: "#585658", padding: 15, margin: 5}}>
             <img style={{height: 384, width: 216}} src={props.imagem1}/>
-            <h4 style={{textAlign: 'center',textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75), -2px -2px 4px rgba(0, 0, 0, 0.75)', color: 'white'}}>{props.titulo1}</h4>
-            <button style={{width: 216}} role="link" onClick={() => abreDesenho(props.imagem1Link)}>Ver Desenho</button>
+            <h4  style={{textAlign: 'center',textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75), -2px -2px 4px rgba(0, 0, 0, 0.75)', color: 'white'}}>{props.titulo1}</h4>
+            <button id={props.id1} style={{width: 216}} role="link" onMouseOut={() => DiminuirBotao(props.id1)} onMouseOver={() => AumentarBotao(props.id1)} onClick={() => abreDesenho(props.imagem1Link)}>Ver Desenho</button>
           </div>
           <div style={{backgroundColor: "#585658", padding: 15, margin: 5}}>
             <img style={{height: 384, width: 216}} src={props.imagem2}/>
             <h4 style={{textAlign: 'center',textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75), -2px -2px 4px rgba(0, 0, 0, 0.75)', color: 'white'}}>{props.titulo2}</h4>
-            <button style={{width: 216}} role="link" onClick={() => abreDesenho(props.imagem2Link)}>Ver Desenho</button>
+            <button id={props.id2} style={{width: 216}} role="link" onMouseOut={() => DiminuirBotao(props.id2)} onMouseOver={() => AumentarBotao(props.id2)} onClick={() => abreDesenho(props.imagem2Link)}>Ver Desenho</button>
           </div>
           <div style={{backgroundColor: "#585658", padding: 15, margin: 5}}>
             <img style={{height: 384, width: 216}} src={props.imagem3}/>
             <h4 style={{textAlign: 'center',textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75), -2px -2px 4px rgba(0, 0, 0, 0.75)', color: 'white'}}>{props.titulo3}</h4>
-            <button style={{width: 216}} role="link" onClick={() => abreDesenho(props.imagem3Link)}>Ver Desenho</button>
+            <button id={props.id3} style={{width: 216, height: 25}} role="link" onMouseOver={() => AumentarBotao(props.id3)} onMouseOut={() => DiminuirBotao(props.id3)} onClick={() => abreDesenho(props.imagem3Link)}>Ver Desenho</button>
           </div>
           <div style={{backgroundColor: "#585658", padding: 15, margin: 5}}>
             <img style={{height: 384, width: 216}} src={props.imagem4}/>
             <h4 style={{textAlign: 'center',textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75), -2px -2px 4px rgba(0, 0, 0, 0.75)', color: 'white'}}>{props.titulo4}</h4>
-            <button style={{width: 216}} role="link" onClick={() => abreDesenho(props.imagem4Link)}>Ver Desenho</button>
+            <button id={props.id4} style={{width: 216}} role="link" onMouseOver={() => AumentarBotao(props.id4)} onMouseOut={() => DiminuirBotao(props.id4)} onClick={() => abreDesenho(props.imagem4Link)}>Ver Desenho</button>
           </div>
           <div style={{backgroundColor: "#585658", padding: 15, margin: 5}}>
             <img style={{height: 384, width: 216}} src={props.imagem5}/>
             <h4 style={{textAlign: 'center',textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75), -2px -2px 4px rgba(0, 0, 0, 0.75)', color: 'white'}}>{props.titulo5}</h4>
-            <button style={{width: 216}} role="link" onClick={() => abreDesenho(props.imagem5Link)}>Ver Desenho</button>
+            <button id={props.id5} style={{width: 216}} role="link" onMouseOver={() => AumentarBotao(props.id5)} onMouseOut={() => DiminuirBotao(props.id5)} onClick={() => abreDesenho(props.imagem5Link)}>Ver Desenho</button>
           </div>
         </div>
       </div>
